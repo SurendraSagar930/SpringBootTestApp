6c53eeaccdd8979dc337a1a0715b7ec5812906c8
Surendra <surendrasagar218@gmail.com>
2022-03-25 13:13:37 +0530

    First Commit

---
+++ src/main/java/com/suri/runner/HelloRunner.java
@@ -0,0 +1,15 @@
+package com.suri.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Spring Boot");
+	}
+
+}
